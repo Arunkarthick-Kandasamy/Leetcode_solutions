16be7fa1e4d61efb1b4f6e64cf38fce08f3c8f4d
Arunkarthick.K <157474398+Arunkarthi123@users.noreply.github.com>
2024-12-21 06:02:05 +0530

    Time: 119 ms (79.27%), Space: 40.8 MB (10.98%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.py
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.py
@@ -1,31 +1,20 @@
 class Solution:
-    def maxKDivisibleComponents(self, n, edges, values, k) -> int:
-
-        if n <= 1 : return 1
-        
-        count = 0
-
-        # build adjacency map
-        G = defaultdict(set)
-        for u,v in edges: G[u].add(v), G[v].add(u)
-        
-        # start with leaves
-        Q = deque(u for u, vs in G.items() if len(vs) == 1)
-        
-        # cut leaves layer by layer
-        while Q:
-            for _ in range(len(Q)):
-                u = Q.popleft()
-                
-                # get u's parent and remove u from its children
-                p = next(iter(G[u])) if G[u] else -1
-                if p >= 0 : G[p].remove(u)
-                
-                # either separate a correct component or add to parent
-                if values[u] % k == 0 : count += 1
-                else                  : values[p] += values[u]
-
-                # update queue with new leaves
-                if p >= 0 and len(G[p]) == 1 : Q.append(p)
-
-        return count
+    def maxKDivisibleComponents(self, n: int, edges: List[List[int]], values: List[int], k: int) -> int:
+        adj = defaultdict(list)
+        src = 0
+        for u,v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        comp = 0
+        def dfs(root,parent):
+            nonlocal comp
+            ans = values[root]
+            for neigh in adj[root]:
+                if parent != neigh:
+                    ans += dfs(neigh,root)
+            if ans % k == 0:
+                comp += 1
+                return 0
+            return ans % k
+        dfs(src,-1)
+        return comp
