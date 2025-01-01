a243e96c23a1b71504051a322e68ab6e56b84cef
Arunkarthick.K <157474398+Arunkarthi123@users.noreply.github.com>
2025-01-01 11:09:12 +0530

    Time: 1 ms (97.36%), Space: 41.8 MB (18.04%) - LeetHub

---
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxScore(String s) {
+        int result=0;
+        int count = 0;
+        int left =0;
+        int right = 0;
+        for(int i = 0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='0')
+            {
+                left++;
+            }
+        }
+        for(int i = s.length()-1;i>0;i--)
+        {
+            if(s.charAt(i)=='1')
+            {
+                right++;
+            }
+            else
+            {
+                left--;
+            }
+            if(result<left+right)
+            {
+                result=left+right;
+            }
+        }
+        return result;
+        
+    }
+}
