a59f2ec8ecac0a916e68f11ed92d705129f4b2dd
Arunkarthick.K <157474398+Arunkarthi123@users.noreply.github.com>
2025-01-07 09:21:54 +0530

    Time: 3 ms (64.01%), Space: 17.6 MB (28.23%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(words)):
             for j in range(len(words)):
                 if words[i] in words[j] and i!=j and words[i] not in result:
-                    print(i," ",j)
                     result.append(words[i])
         return result
 
