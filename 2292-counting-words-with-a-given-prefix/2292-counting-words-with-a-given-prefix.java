fde6727d9724aeb784122a45d564f534e752c43c
Arunkarthick.K <157474398+Arunkarthi123@users.noreply.github.com>
2025-01-09 06:32:05 +0530

    Time: 0 ms (100%), Space: 43.1 MB (26.48%) - LeetHub

--- 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
+++ 2292-counting-words-with-a-given-prefix/2292-counting-words-with-a-given-prefix.java
@@ -1,25 +1,10 @@
 class Solution {
     public int prefixCount(String[] w, String p) {
-        int n = p.length() , count = 0;
+        int count = 0;
         for(int i=0;i<w.length;i++)
         {
-            if(w[i].length()<n)
-            {
-                continue;
-            }
-            boolean flag=true;
-            for(int j=0;j<n;j++)
-            {
-                if(p.charAt(j)!=w[i].charAt(j))
-                {
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag)
-            {
+            if(w[i].startsWith(p))
                 count++;
-            }
         }
         return count;        
     }
