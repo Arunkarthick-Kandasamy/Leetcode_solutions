5eb49cb53c9d05c283b1464aa725a9fb314626e7
Arunkarthick.K <157474398+Arunkarthi123@users.noreply.github.com>
2025-01-13 07:52:29 +0530

    Time: 9 ms (92.05%), Space: 46.8 MB (24.5%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -2,7 +2,6 @@ class Solution {
     public int minimumLength(String s) {
         int n=s.length();
         int[] hash=new int[26];
-        int[] arr=new int[n];
         for(int i=0;i<n;i++)
         {
             char ch=s.charAt(i);
